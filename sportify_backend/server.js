0c129900cd914e8c31272fb233eea5f86b80218f
burhan-shahzad-1 <burhanshahzad135@gmail.com>
2025-11-23 16:58:01 +0500

    Manage account and Logos

--- sportify_backend/server.js
+++ sportify_backend/server.js
@@ -52,7 +52,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Sportify Backend Server running on port ${PORT}`);
